b18b6e41f4c84cf2f14b2642f9b92b12ee56344d
Jayson <jrf@0ptimize.com>
2025-05-25 17:20:26 -0400

    fix: switch session strategy to database for persistent session cookie

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -76,7 +76,7 @@ export const authOptions: NextAuthOptions = {
     })
   ],
   session: {
-    strategy: "jwt",
+    strategy: "database",
     maxAge: 30 * 24 * 60 * 60, // 30 days
     updateAge: 24 * 60 * 60, // 24 hours
   },
